6f51bad1ffc7e418a713a0794f62524238fcfe34
ObieWalker <obinnawalker@gmail.com>
2019-11-01 12:43:11 +0100

    project setup

---
+++ cmd/brix_flix/models/base.go
@@ -0,0 +1,41 @@
+package models
+
+import (
+	_ "github.com/jinzhu/gorm/dialects/postgres"
+	"github.com/jinzhu/gorm"
+	"os"
+	"github.com/joho/godotenv"
+	"fmt"
+)
+
+var db *gorm.DB //database
+
+func init() {
+
+	e := godotenv.Load() //Load .env file
+	if e != nil {
+		fmt.Print(e)
+	}
+
+	username := os.Getenv("DB_USERNAME")
+	password := os.Getenv("DB_PASSWORD")
+	dbName := os.Getenv("DB_NAME")
+	dbHost := os.Getenv("DB_HOST")
+
+
+	dbUri := fmt.Sprintf("host=%s user=%s dbname=%s sslmode=disable password=%s", dbHost, username, dbName, password) //Build connection string
+	fmt.Println(dbUri)
+
+	conn, err := gorm.Open("postgres", dbUri)
+	if err != nil {
+		fmt.Print(err)
+	}
+
+	db = conn
+	db.Debug().AutoMigrate(&Account{}, &Contact{}) //Database migration
+}
+
+//returns a handle to the DB object
+func GetDB() *gorm.DB {
+	return db
+}
