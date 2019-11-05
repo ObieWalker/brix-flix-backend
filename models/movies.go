711517c145c9a917338f1c1be7f7f8658d76d582
ObieWalker <obinnawalker@gmail.com>
2019-11-05 14:53:35 +0100

    fix request param error

--- models/movies.go
+++ models/movies.go
@@ -1,14 +1,26 @@
 package models
 
 import (
-	_ "github.com/jinzhu/gorm/dialects/postgres"
+	"brix-flix-backend/utils"
 	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/postgres"
 )
 
-type Movies struct {
+type Movie struct {
 	gorm.Model
 	Name string `gorm:"column:name" json:"name"`
 	Year  string `gorm:"column:year" json:"year"`
 	Description   string `gorm:"column:description" json:"description"`
 }
 
+func (movie *Movie) Create() (map[string] interface{}) {
+	GetDB().Create(movie)
+
+	if movie.ID <= 0 {
+		return utils.Message(false, "Failed to create movie, connection error.")
+	}
+
+	response := utils.Message(true, "Movie has been added.")
+	response["movie"] = movie
+	return response
+}
