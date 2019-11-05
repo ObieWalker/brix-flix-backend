60e3895e8ee0cf7d4fe52a796ee028cf77bdaba0
topseySuave <gabrielsuave17@gmail.com>
2019-11-06 00:13:04 +0100

    [alpha-0.0.1] Remove commented code and Gopkg file

--- cmd/brix-flix-backend/main.go
+++ cmd/brix-flix-backend/main.go
@@ -7,20 +7,7 @@ import (
 		"net/http"
 		"brix-flix-backend/controllers"
 		"brix-flix-backend/models"
-
-    // importing any module from the root should
-    // be easier to do e.g: models "brix_flix_server/models"
 )
-
-// func homePage(w http.ResponseWriter, r *http.Request){
-//     fmt.Fprintf(w, "Welcome to the HomePage!")
-//     fmt.Println("Endpoint Hit: homePage")
-// }
-
-// func handleRequests() {
-//     http.HandleFunc("/", homePage)
-//     log.Fatal(http.ListenAndServe(":10000", nil))
-// }
 
 func newRouter() *mux.Router {
 	router := mux.NewRouter()
